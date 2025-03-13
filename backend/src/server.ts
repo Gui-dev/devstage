dddf66dbd5d2337dbdad2bbcd909573f55e661bc
Gui-dev <adrianon10@gmail.com>
2025-03-13 04:22:11 -0300

    chore: add swagger config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,17 @@
 import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
 
-const app = fastify()
+import { createSubscriptionRoute } from './modules/subscriptions/routes/create-subscription.route'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 const PORT = Number(process.env.PORT) || 3333
 
 app.setSerializerCompiler(serializerCompiler)
@@ -13,6 +19,21 @@ app.setValidatorCompiler(validatorCompiler)
 app.register(fastifyCors, {
   origin: 'http://localhost:3000',
 })
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Devstage API',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(createSubscriptionRoute)
 
 app.listen({ port: PORT, host: '0.0.0.0' }, () => {
   console.log(`HTTP server running on http://localhost:${PORT}`)
