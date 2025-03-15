d9dce2aa51c9b1677d44969604420b2fd889acae
Gui-dev <adrianon10@gmail.com>
2025-03-15 04:27:24 -0300

    feat: add check if email exists

--- backend/src/modules/subscriptions/use-cases/create-subscription.ts
+++ backend/src/modules/subscriptions/use-cases/create-subscription.ts
@@ -1,3 +1,4 @@
+import { eq } from 'drizzle-orm'
 import { db } from '../../../shared/databases/drizzle/client'
 import { subscriptions } from '../../../shared/databases/drizzle/schemas/subscriptions'
 
@@ -8,6 +9,17 @@ interface ICreateSubscriptionUseCaseProps {
 
 export class CreateSubscriptionUseCase {
   public async execute({ name, email }: ICreateSubscriptionUseCaseProps) {
+    const subscribers = await db
+      .select()
+      .from(subscriptions)
+      .where(eq(subscriptions.email, email))
+
+    if (subscribers.length > 0) {
+      return {
+        subscriber_id: subscribers[0].id,
+      }
+    }
+
     const result = await db
       .insert(subscriptions)
       .values({
