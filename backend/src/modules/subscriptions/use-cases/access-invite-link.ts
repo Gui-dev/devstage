eebb4d86354082dcce9416093d98251a2b7ccd5b
Gui-dev <adrianon10@gmail.com>
2025-03-15 04:20:41 -0300

    feat: add access invite link use case

---
+++ backend/src/modules/subscriptions/use-cases/access-invite-link.ts
@@ -0,0 +1,11 @@
+import { redis } from '../../../shared/databases/redis/client'
+
+interface IAccessInviteLinkUseCase {
+  subscriber_id: string
+}
+
+export class AccessInviteLinkUseCase {
+  public async execute({ subscriber_id }: IAccessInviteLinkUseCase) {
+    await redis.hincrby('referral:access-count', subscriber_id, 1)
+  }
+}
