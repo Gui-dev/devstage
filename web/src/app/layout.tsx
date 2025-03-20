14650ecb2b0f1d0a495cb7bc708c849fab6d6166
Gui-dev <adrianon10@gmail.com>
2025-03-20 04:56:55 -0300

    chore: add setup initial

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next'
+import { Montserrat, Oxanium } from 'next/font/google'
+
+import './global.css'
+
+const oxanium = Oxanium({
+  weight: ['500', '600'],
+  subsets: ['latin'],
+  variable: '--font-oxanium',
+})
+
+const montserrat = Montserrat({
+  weight: ['400', '600'],
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+})
+
+export const metadata: Metadata = {
+  title: 'Devstage App',
+  description: 'Devstage app',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" className={`${oxanium.variable} ${montserrat.variable}`}>
+      <body className="bg-gray-900">{children}</body>
+    </html>
+  )
+}
---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="text-4xl text-gray-100">Hello world!</div>
+    </main>
+  )
+}
