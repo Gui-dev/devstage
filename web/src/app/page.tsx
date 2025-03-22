c047ce31716c2fcd452f3a59e9d790049a4a4c80
Gui-dev <adrianon10@gmail.com>
2025-03-22 05:09:54 -0300

    feat: add home page styles

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,25 +1,72 @@
+import logo from '@/assets/logo.svg'
 import { Button } from '@/components/button'
-import { IconButton } from '@/components/icon-button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
-import { ArrowRight, Mail } from 'lucide-react'
+import { ArrowRight, Mail, Radio, User } from 'lucide-react'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-6">
-      <Button>
-        Enviar
-        <ArrowRight />
-      </Button>
-      <IconButton>
-        <ArrowRight />
-      </IconButton>
+    <main className="mx-auto max-w-[1240px] px-5 py-8 md:py-0">
+      <div className="flex min-h-dvh flex-col justify-center gap-16">
+        <div className="flex w-full max-w-[550px] flex-col items-center gap-8 md:items-start">
+          <Image src={logo} alt="Devstage Logo" height={30} width={108.5} />
+          <h1 className="flex flex-col text-center font-heading font-medium text-4xl leading-none md:text-left md:text-7xl">
+            <span className="text-blue">CodeCraft</span> Summit 2025
+          </h1>
+        </div>
 
-      <InputRoot>
-        <InputIcon>
-          <Mail />
-        </InputIcon>
-        <InputField type="email" placeholder="Digite seu e-mail" />
-      </InputRoot>
+        <div className="flex flex-col items-stretch gap-5 md:flex-row">
+          <div className="flex-1 space-y-6 rounded-2xl border border-gray-600 bg-gray-700 p-8">
+            <div className="flex items-center justify-between">
+              <h2 className="font-heading font-semibold text-gray-200 text-xl">
+                Sobre o evento
+              </h2>
+              <span className="flex items-center gap-2 font-semibold text-purple text-xs">
+                <Radio className="size-5" />
+                AO VIVO
+              </span>
+            </div>
+
+            <p className="text-gray-300 text-sm leading-relaxed md:text-base">
+              Um evento feito por para pessoas desenvolvedoras apaixonadas por
+              criar soluções inovadoras e compartilhar conhecimento. Vamos
+              mergulhar nas tendências mais recentes em desenvolvimento de
+              software, arquitetura de sistemas e tecnologias emergentes, com
+              palestras, workshops e hackathons.
+              <br />
+              <br />
+              Dias 15 a 17 de março | Das 18h às 21h | online & Gratuito
+            </p>
+          </div>
+
+          <form className="w-full space-y-6 rounded-2xl border border-gray-600 bg-gray-700 p-8 md:max-w-[440px]">
+            <h2 className="font-heading font-semibold text-gray-200 text-xl">
+              Inscrição
+            </h2>
+
+            <div className="space-y-3">
+              <InputRoot>
+                <InputIcon>
+                  <User />
+                </InputIcon>
+                <InputField type="text" placeholder="Nome completo" />
+              </InputRoot>
+
+              <InputRoot>
+                <InputIcon>
+                  <Mail />
+                </InputIcon>
+                <InputField type="email" placeholder="E-mail" />
+              </InputRoot>
+            </div>
+
+            <Button type="submit">
+              Confirmar
+              <ArrowRight />
+            </Button>
+          </form>
+        </div>
+      </div>
     </main>
   )
 }
