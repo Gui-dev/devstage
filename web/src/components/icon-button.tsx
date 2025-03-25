beed04e80fdceec397dc04aa2a03f75f78482332
Gui-dev <adrianon10@gmail.com>
2025-03-25 05:12:23 -0300

    feat: add tailwind merge

--- web/src/components/icon-button.tsx
+++ web/src/components/icon-button.tsx
@@ -1,13 +1,21 @@
 import type { ComponentProps, ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface IIconButtonProps extends ComponentProps<'button'> {
   children: ReactNode
 }
 
-export const IconButton = ({ children, ...rest }: IIconButtonProps) => {
+export const IconButton = ({
+  children,
+  className,
+  ...rest
+}: IIconButtonProps) => {
   return (
     <button
-      className="cursor-pointer rounded-md bg-gray-500 p-1.5 text-blue transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      className={twMerge(
+        'cursor-pointer rounded-md bg-gray-500 p-1.5 text-blue transition-colors duration-300 hover:bg-blue hover:text-gray-900',
+        className
+      )}
       {...rest}
     >
       {children}
